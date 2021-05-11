6fa397155e8979ebd8a80ed5a69d78df0b8de4e1
RubenV-Dev <r.will16@hotmail.com>
2021-05-11 18:06:40 -0400

    getAutos service test passed with assertion that autoslist is not empty

--- src/test/java/com/galvanize/autos/AutosServiceTest.java
+++ src/test/java/com/galvanize/autos/AutosServiceTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class AutosServiceTest {
     private AutosService autosService;
@@ -23,8 +27,11 @@ class AutosServiceTest {
 
     @Test
     void getAutos() {
+        Automobile automobile = new Automobile("Toyota", 1994, "Camry", "44444");
+        when(autosRepository.findAll()).thenReturn(Arrays.asList(automobile));
         AutosList autoslist = autosService.getAutos();
         assertThat(autoslist).isNotNull();
+        assertThat(autoslist.isEmpty() == false);
     }
 
     @Test
